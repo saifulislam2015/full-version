128b35fa445ce24315db449df06ffc8659d8a5f9
Saiful Islam <saifulislamt073@gmail.com>
2022-05-30 23:16:39 +0600

    Updates

---
+++ static/js/20.f793c45a.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[20],{991:function(a,s,d){}}]);
+//# sourceMappingURL=20.f793c45a.chunk.js.map
